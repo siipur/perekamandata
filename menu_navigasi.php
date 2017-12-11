55c1616cd5bf738cf7fb972c05fb65f03aa94604
DIDI PURNOMO <33234547+siipur@users.noreply.github.com>
2017-12-12 04:20:21 +0700

    Create menu_navigasi.php

---
+++ menu_navigasi.php
@@ -0,0 +1,11 @@
+<ul class="" style="display:blok">
+	<li><a href="?page=home">Home</a></li>	
+	<li>
+		<a href="#">Master</a>
+		<ul>
+			<li><a href="?page=user">Kelola User</a></li>
+			<li><a href="?page=warga">Kelola Data Warga</a></li>
+		</ul>
+	</li>
+	<li><a href="logout.php">Keluar (<b><i>logout</i></b>)</a></li>
+</ul>
