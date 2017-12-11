e50f74c791eb8276306c4e8c7c381ab9bb5af1c5
DIDI PURNOMO <33234547+siipur@users.noreply.github.com>
2017-12-12 03:51:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>Halaman awal</title>
+</head>
+<body>
+	<p>Ini Halaman Yang ditampilkan dari index.php</p>
+	<p><strong>index.php</strong> merupakan default file yang akan dipanggil pertama kali pada ssat kita menjalankan aplikasi pertama kali</p>
+	<p><em>Hallo world</em></p>
+	<p>pada kesempatan kali ini kita akan belajar bahasa pemprograman PHP yang merupakan bahasa pemprograman yang sering digunakan dalam pembuatan web/website.</p>
+	<p>
+		sebelum mengenal PHP alangkah lebih baiknya kita belajar HTML dulu yah (sebagai pondasi dalam penulisan teks). dan Nanti kita akan menambahkan tampilan
+		dengan CSS dan sedikit polesan dengan Javascript. Baru setelah itu Perkuat dasar Pemprograman PHP.<br/>
+		<code>GOOD LUCK</code>
+	</p>
+	<div class="materi">
+		Aplikasi ini merupakan sistem pendataan warga, yang hanya terdiri dari 2 tabel dalam satu database, tabel tersebut antara lain untuk proses login (tbl_user) 
+		dan untuk pendataan data warga (tbl_ktp).<br/>
+
+		Apa aja yang akan dipelajari dari Aplikasi ini merupakan level dasar dari penggunan bahasa pemprograman PHP, materinya antara lain sebagai berikut:<br/> 
+		<ul>
+			<li>Mengkoneksikan Aplikasi dengan Database Server MariaDB atau MySQL</li>
+			<li>Pembuatan form login dan validasi (pengecekan login)</li>
+			<li>CREATE (proses membuat/memasukan data)</li>
+			<li>READ (proses menampilkan data)</li>
+			<li>UPDATE (proses pengeditan data)</li>
+			<li>DELETE (proses penghapusan data)</li>
+			<li>Pembuatan form pencarian</li>
+			<li>Pembuatan pagination</li>
+			<li>proses Upload gambar</li>
+			<li>Mencetak data ke PDF dengan library DOMPDF</li> 
+		</ul>	
+
+		OK, kurang lebih segitu yang akan dipelajari dari aplikasi ini. Selamat Belajar!!.<br/>
+		dan jangan lupa kunjungi blog <a href="http://curansongho.blogspot.com" target="_blank">CURANSONGHO</a> untuk mendapatkan informasi mengenai curahan kehidupan.<br/>
+		dan juga kunjungi <a href="http://jakwirtkj.blogspot.com" target="_blank">JAKWIRTKJ</a> untuk mengetahui seputar teknologi informasi dan jaringan<br/>
+		Terimakasih.<br/>
+	</div>
+	<hr/>
+	<button><a href="login.php">SILAHKAN LOGIN</a></button> 
+</body>
+</html>
